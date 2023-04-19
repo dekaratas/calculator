4f209dcb6d870af95c44f32a9f5036ab2cb81e6a
Dennis <thebr1dgeburner@proton.me>
2023-04-19 22:43:06 +0200

    Declare functions for mathmatical operations

---
+++ script.js
@@ -0,0 +1,16 @@
+// Function declaration for the mathmatical operations
+function add(a,b ) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
