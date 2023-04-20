6a6b6b8f0608024b91ef3c704343c43caea5a36d
Dennis <thebr1dgeburner@proton.me>
2023-04-20 18:45:07 +0200

    Implement Add functionality

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 // Global Query Selectors
 const numBtn = document.querySelectorAll(".numPad");
 let textCont = document.querySelector("#text");
+const addBtn = document.querySelector("#add");
+const subBtn = document.querySelector("#subtract");
+const multBtn = document.querySelector("#multiply");
+const divBtn = document.querySelector("#divide");
+const eqBtn = document.querySelector("#equal");
+const canBtn = document.querySelector("#cancel");
+
+let a = 0;
+let op = "";
+let b = 0;
+
 
 // Adjust content of display on each button press
 // Concatenates button's text content to display text
 numBtn.forEach(button => {
   button.addEventListener("click", event => {
-    textCont.textContent += `${button.textContent}`;
-    console.log("Button CLICK");
-    console.log(`${button.textContent}`);
+    if (button.textContent != "C" && button.textContent != "=") {
+    textCont.textContent += `${button.textContent}`;}
   });
 });
+
+addBtn.addEventListener("click", () =>  {
+  a = parseInt(text.textContent);
+  op = "+";
+  textCont.textContent = "";
+});
+
+eqBtn.addEventListener("click", () => {
+  b = parseInt(text.textContent);
+  switch(op) {
+    case "+":
+      textCont.textContent = a+b;
+      break;
+  }
+});
+
 
 
 // Function declaration for the mathmatical operations
 function add(a,b ) {
-  return a + b;
+  return a+b;
 }
 
 function subtract(a, b) {
